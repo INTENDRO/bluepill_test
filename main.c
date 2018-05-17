a18bf67457edb8f06f47b701bfd9147db849ceb7
Mario Noseda <mnoseda@bluewin.ch>
2018-05-18 00:16:31 +0200

    wait_1ms added

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stm32f10x.h>
 
+void wait_1ms(uint16_t u16Factor) // using timer 4
+{
+	uint16_t i;
+	
+	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	TIM4->CR1 = 0x0000;
+	TIM4->CR2 = 0x0000;
+	TIM4->SMCR = 0x0000;
+	TIM4->DIER = 0x0000;
+	TIM4->CCMR1 = 0x0000;
+	TIM4->CCMR2 = 0x0000;
+	TIM4->CCER = 0x0000;
+	TIM4->PSC = 1;
+	TIM4->ARR = 35999;
+	TIM4->EGR = TIM_EGR_UG;
+	
+	TIM4->SR &= ~TIM_SR_UIF;
+	TIM4->CR1 |= TIM_CR1_CEN;
+	
+	for(i=0;i<u16Factor;i++)
+	{
+		while(!(TIM4->SR & TIM_SR_UIF));
+		TIM4->SR &= ~TIM_SR_UIF;
+	}
+	TIM4->CR1 = 0x0000;
+}
 
 int main(void)
 {
@@ -8,9 +34,16 @@ int main(void)
 	RCC->APB2ENR = RCC_APB2ENR_IOPCEN;
 	
 	GPIOC->CRH = GPIO_CRH_MODE13_0;
-	GPIOC->ODR = 0;
+	GPIOC->ODR = GPIO_ODR_ODR13;
 	
 	
 	
-	while(1);
+	while(1)
+	{
+		GPIOC->BSRR = GPIO_BSRR_BS13;
+		wait_1ms(1000);
+		GPIOC->BSRR = GPIO_BSRR_BR13;
+		wait_1ms(1000);
+		
+	}
 }
